c8c5ac7de55fc1b901b73a53d0e4fac114fea720
Harry <harrison2.hibbins@live.uwe.ac.uk>
2021-10-27 11:00:08 +0100

    Changed font

---
+++ Overcooked Demake/Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text ScoreText;
+    public int score;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
