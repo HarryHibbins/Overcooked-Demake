ed74e925b05b04f9d33dbf0c18ba61b5e2e23887
Harry <harrison2.hibbins@live.uwe.ac.uk>
2021-10-27 13:44:16 +0100

    started blocking movement if object is in the way

--- Overcooked Demake/Assets/Scripts/PlayerMovement.cs
+++ Overcooked Demake/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,8 +10,13 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private float horiztonal;
     private float vertical;
+    public int moveUnits;
 
-    // Start is called before the first frame update
+    public GameObject RaycastObject;
+    public LayerMask Environemnt;
+    private bool ObjectBlocking;
+    
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -23,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horiztonal = Input.GetAxis("Horizontal");
+        /*horiztonal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
         if (horiztonal != 0)
@@ -35,8 +41,67 @@ public class PlayerMovement : MonoBehaviour
             horiztonal = 0;
         }
 
-        rb.velocity = new Vector2(horiztonal, vertical) * speed;
+        rb.velocity = new Vector2(horiztonal, vertical) * speed;*/
+
+    
+
+        
+     
+
+        Debug.DrawRay(RaycastObject.transform.position, RaycastObject.transform.TransformDirection(Vector3.up) , Color.red);
+        
+      
+        RaycastHit2D hit =  Physics2D.Raycast(RaycastObject.transform.position, RaycastObject.transform.TransformDirection(Vector3.up) ,
+            1, Environemnt);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit" + hit.collider.name);
+            ObjectBlocking = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (!ObjectBlocking)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + moveUnits,
+                    transform.position.z);
+
+                transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - moveUnits, transform.position.y ,
+                transform.position.z);
+            
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 90);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - moveUnits,
+                transform.position.z);
+            
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 180);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + moveUnits, transform.position.y ,
+                transform.position.z);
+            
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 270);
+
+        }
+
+        ObjectBlocking = false;
 
 
     }
+
 }
